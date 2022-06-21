67db2537c194a290a589e42edf2dd25dc7abf149
ch0992 <choilee6@naver.com>
2022-06-21 21:07:37 +0900

    asdf

--- src/nomad_coder/functions2.ts
+++ src/nomad_coder/functions2.ts
@@ -1,7 +1,9 @@
+//concretetype
 type SuperPrint = {
     (arr: number[]):void
     (arr: boolean[]):void
     (arr: string[]):void
+    (arr: (number|boolean)[]):void
 }
 
 const superPrint: SuperPrint = (arr) => {
@@ -11,3 +13,25 @@ const superPrint: SuperPrint = (arr) => {
 superPrint([1,2,3,4])
 superPrint([true,false,true])
 superPrint(["a","b","c"])
+superPrint([1,2,true,false])
+superPrint([1,2])
+
+
+//==========================================
+
+//generic
+type SuperPrint2 = {
+    <T>(arr: T[]):void
+}
+
+const superPrint2: SuperPrint2 = (arr) => {
+    arr.forEach(i=>console.log(i))
+}
+
+superPrint2([1,2,3,4])
+superPrint2([true,false,true])
+superPrint2(["a","b","c"])
+superPrint2([1,2,true,false])
+superPrint2([1,2,3,4,5,6,7,false,"asdf"])
+
+
